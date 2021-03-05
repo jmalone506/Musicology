3798f6e900b3b279bab672058312a6550240f1f9
Joshua Malone <jcharlesmalone@gmail.com>
2021-03-04 21:46:06 -0800

    models playlist

---
+++ sequelize-passport-example/models/playlist.js
@@ -0,0 +1,23 @@
+
+var Sequelize = require("sequelize");
+var sequelize = require("../config/connection.js");
+
+var Playlist = sequelize.define("playlist", {
+  routeName: Sequelize.STRING,
+  genre: Sequelize.STRING,
+  song: Sequelize.STRING,
+  artist: Sequelize.STRING,
+  playlist: Sequelize.STRING,
+  album: Sequelize.STRING,
+  search: Sequelize.ARRAY
+  
+  
+}, {
+
+  freezeTableName: true
+});
+
+
+Playlist.sync();
+
+module.exports = Playlist
